0b59296b941e27df569846dc0bf533209d19b3a5
Gary <26419401+Gary-H9@users.noreply.github.com>
2022-06-10 14:32:48 +0100

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,11 @@ echo "Checking for LICENSE file Existence..."
 
 #test -f LICENSE #&& echo "LICENSE exists."
 
-# if `test -f LICENSE` ; then
-#     echo "LICENSE File Exists."
-# else
-#     echo "LICENSE File DOES NOT Exist."
-# fi
+if `test -f LICENSE` ; then
+    echo "LICENSE File Exists."
+else
+    echo "LICENSE File DOES NOT Exist."
+    exit 1
+fi
 
-test -f LICENSE && echo "LICENSE File Exists." || echo "LICENSE File DOES NOT Exist." exit 1
+# test -f LICENSE && echo "LICENSE File Exists." || echo "LICENSE File DOES NOT Exist." exit 1
